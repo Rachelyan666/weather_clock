3149a512dfb6ab171af20228cc03706857256f60
Rachel Yan <sy625@cornell.edu>
2025-07-18 16:51:47 -0400

    make changes for dma cleanups

--- stm32_dma/app/main.c
+++ stm32_dma/app/main.c
@@ -6,47 +6,64 @@
 #include "led.h"
 #include "usart.h"
 
-
-// static uint8_t data_buffer[16];
-// static uint16_t data_len;
+static uint8_t data_buffer[16];
+static uint16_t data_len;
 static bool usart_tx_busy = false;
 
+static void usart_received(uint8_t data)
+{
+	if (data_len < 15)
+	{
+		data_buffer[data_len] = data;
+		data_len++;
+	}
+}
 
-// static void usart_received(uint8_t data){
-// 	if (data_len < 15){
-// 		data_buffer[data_len] = data;
-// 		data_len++;
-// 	}
-// }
-
-static void wait_usart_send_finish(void){
+static void on_usart_send_finish(void)
+{
 	usart_tx_busy = false;
 }
 
-static void usart_send_finish(void){
-	while (usart_tx_busy == true);
+static void wait_usart_send_finish(void)
+{
+	while (usart_tx_busy == true)
+		;
 }
 
-int main(void){
+#define write_string(_str)           \
+	do                                 \
+	{                                  \
+		usart_tx_busy = true;            \
+		usart_write_string(_str, false); \
+		wait_usart_send_finish();        \
+	} while (0);
+
+#define write_data(_data, _len)           \
+	do                                      \
+	{                                       \
+		usart_tx_busy = true;                 \
+		usart_write_data(_data, _len, false); \
+		wait_usart_send_finish();             \
+	} while (0);
+
+int main(void)
+{
 
 	board_init();
 	led_init();
 	usart_init();
-	//usart_send_finish_register(usart_send_finish);
-	//usart_receive_register(usart_received);
+	usart_send_finish_register(on_usart_send_finish);
+	usart_receive_register(usart_received);
 
-	const char str[] = "hello dma2\n";
-	while(true){
-		usart_tx_busy = true;
-		usart_write_async((uint8_t *)str, strlen(str));
-		//wait_usart_send_finish();
-		//delay(1000);
+	while (true)
+	{
 
-		// if (data_len > 0){
-		// 	usart_write_string("Received: ");
-		// 	usart_write_data(data_buffer, data_len);
-		// 	data_len = 0;
-		// 	usart_write_string("\r\n");
-		// }
+		if (data_len > 0)
+		{
+			write_string("Received: ");
+			write_data(data_buffer, data_len);
+			write_string("\r\n");
+			data_len = 0;
+		}
 	}
 }
--- stm32_dma/app/usart.c
+++ stm32_dma/app/usart.c
@@ -1,36 +1,32 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-#include "usart.h"
 #include "stm32f10x.h"
+#include "usart.h"
 
-
-#define LED_PORT GPIOC
-#define LED_PIN GPIO_Pin_13
-
-#define USART_PORT GPIOA
-#define TX_PIN GPIO_Pin_9
-#define RX_PIN GPIO_Pin_10
-
-static usart_receive_callback_t usart_receive_callback;
-static usart_send_finish_callback_t usart_send_finish_callback;
 static volatile bool usart_send_busy;
+static usart_send_finish_callback_t usart_send_finish_callback;
+static usart_receive_callback_t usart_receive_callback;
 
-
-void usart_init(void){
+static void usart_gpio_init(void)
+{
 	GPIO_InitTypeDef GPIO_InitStructure;
+
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Pin = TX_PIN;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = RX_PIN;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
 
+static void usart_nvic_init(void)
+{
 	NVIC_InitTypeDef NVIC_InitStructure;
-	/* Enable the USART1 Interrupt */
+
 	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 5;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
@@ -42,7 +38,10 @@ void usart_init(void){
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
+}
 
+static void usart_lowlevel_init(void)
+{
 	USART_InitTypeDef USART_InitStructure;
 	USART_InitStructure.USART_BaudRate = 115200;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
@@ -51,23 +50,25 @@ void usart_init(void){
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-	/* Configure USART1 */
 	USART_Init(USART1, &USART_InitStructure);
-	/* Enable USARTy Receive and Transmit interrupts */
-  	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-	/* Enable USARTy DMA TX request */
-  	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
-	/* Enable the USART1 */
-	USART_Cmd(USART1, ENABLE);	
-
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
+	USART_Cmd(USART1, ENABLE);
 }
 
-void usart_write_async(uint8_t *data, uint16_t length){
-	//dma
+void usart_init(void)
+{
+	usart_gpio_init();
+	usart_nvic_init();
+	usart_lowlevel_init();
+}
+
+void usart_write_data(uint8_t *data, uint16_t length, bool async)
+{
+	DMA_DeInit(DMA1_Channel4);
 	DMA_InitTypeDef DMA_InitStructure;
-	DMA_DeInit(DMA1_Channel4); 
-	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t) &USART1->DR;
-	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t) data;
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&USART1->DR;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)data;
 	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
 	DMA_InitStructure.DMA_BufferSize = length;
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
@@ -80,52 +81,51 @@ void usart_write_async(uint8_t *data, uint16_t length){
 	DMA_Init(DMA1_Channel4, &DMA_InitStructure);
 	DMA_ITConfig(DMA1_Channel4, DMA_IT_TC, ENABLE);
 	usart_send_busy = true;
-	/* Enable USART1 DMA TX Channel */
 	DMA_Cmd(DMA1_Channel4, ENABLE);
-	while (usart_send_busy);
-}
 
-void usart_write_data(uint8_t *data, uint16_t length){
-	for (uint16_t i = 0; i < length; i++){
-		while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-		USART_SendData(USART1, data[i]);
+	if (!async)
+	{
+		while (usart_send_busy)
+			;
 	}
 }
 
-void usart_write_string(const char *str){
+void usart_write_string(const char *str, bool async)
+{
 	uint16_t len = strlen(str);
-	usart_write_data((uint8_t *)str, len);
+	usart_write_data((uint8_t *)str, len, async);
 }
 
-void usart_send_finish_register(usart_send_finish_callback_t callback){
+void usart_send_finish_register(usart_send_finish_callback_t callback)
+{
 	usart_send_finish_callback = callback;
 }
 
-void usart_receive_register(usart_receive_callback_t callback){
+void usart_receive_register(usart_receive_callback_t callback)
+{
 	usart_receive_callback = callback;
 }
 
-void DMA1_Channel4_IRQHandler(void){
-	if(DMA_GetITStatus(DMA1_IT_TC4) == SET)
+void DMA1_Channel4_IRQHandler(void)
+{
+	if (DMA_GetITStatus(DMA1_IT_TC4) == SET)
 	{
 		usart_send_busy = false;
-		if (usart_send_finish_callback){
+
+		if (usart_send_finish_callback)
 			usart_send_finish_callback();
-		}
+
 		DMA_ClearITPendingBit(DMA1_IT_TC4);
 	}
 }
 
-void USART1_IRQHandler(void){
-	if(USART_GetITStatus(USART1, USART_IT_RXNE) == SET)
+void USART1_IRQHandler(void)
+{
+	if (USART_GetITStatus(USART1, USART_IT_RXNE) == SET)
 	{
-		/* Read one byte from the receive data register */
 		uint8_t data = USART_ReceiveData(USART1);
 
-		if (usart_receive_callback){
+		if (usart_receive_callback)
 			usart_receive_callback(data);
-		}
 	}
 }
-
-
