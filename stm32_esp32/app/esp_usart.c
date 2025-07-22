78bb573e2ca8c68453d2e5c339689149295a5b8b
Rachel Yan <sy625@cornell.edu>
2025-07-22 00:51:36 -0400

    add receive line

--- stm32_esp32/app/esp_usart.c
+++ stm32_esp32/app/esp_usart.c
@@ -4,71 +4,92 @@
 #include "stm32f10x.h"
 #include "esp_usart.h"
 
-
 static esp_usart_receive_callback_t esp_usart_receive_callback;
-
 
 void esp_usart_init(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure;
-    
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-    
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitTypeDef GPIO_InitStructure;
 
-    NVIC_InitTypeDef NVIC_InitStructure;
-    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 5;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-    
-    USART_InitTypeDef USART_InitStructure;
-    USART_InitStructure.USART_BaudRate = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    
-    USART_Init(USART2, &USART_InitStructure);
-    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
-    USART_Cmd(USART2, ENABLE);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 5;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	USART_Init(USART2, &USART_InitStructure);
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+	USART_Cmd(USART2, ENABLE);
+}
+
+bool receive_line(char *buf, size_t maxlen)
+{
+	size_t len = 0;
+	while (1)
+	{
+		if (USART_GetFlagStatus(USART2, USART_FLAG_RXNE))
+		{
+			char c = USART_ReceiveData(USART2);
+			if (c == '\n')
+			{
+				buf[len] = '\0';
+				return true;
+			}
+			if (len < maxlen - 1)
+			{
+				buf[len++] = c;
+			}
+		}
+	}
+	return false;
 }
 
 void esp_usart_write_data(uint8_t *data, uint16_t length)
 {
-    for (uint16_t i = 0; i < length; i++)
-    {
-        while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
-        USART_SendData(USART2, data[i]);
-    }
+	for (uint16_t i = 0; i < length; i++)
+	{
+		while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET)
+			;
+		USART_SendData(USART2, data[i]);
+	}
 }
 
 void esp_usart_write_string(const char *str)
 {
-    uint16_t len = strlen(str);
-    esp_usart_write_data((uint8_t *)str, len);
+	uint16_t len = strlen(str);
+	esp_usart_write_data((uint8_t *)str, len);
 }
 
 void esp_usart_receive_register(esp_usart_receive_callback_t callback)
 {
-    esp_usart_receive_callback = callback;
+	esp_usart_receive_callback = callback;
 }
 
 void USART2_IRQHandler(void)
 {
-    if(USART_GetITStatus(USART2, USART_IT_RXNE) == SET)
-    {
-        uint8_t data = USART_ReceiveData(USART2);
-        
-        if (esp_usart_receive_callback)
-            esp_usart_receive_callback(data);
-    }
+	if (USART_GetITStatus(USART2, USART_IT_RXNE) == SET)
+	{
+		uint8_t data = USART_ReceiveData(USART2);
+
+		if (esp_usart_receive_callback)
+			esp_usart_receive_callback(data);
+	}
 }
--- stm32_esp32/app/esp_usart.h
+++ stm32_esp32/app/esp_usart.h
@@ -1,17 +1,14 @@
 #ifndef __ESP_USART_H
 #define __ESP_USART_H
 
-
 #include <stdint.h>
-
 
 typedef void (*esp_usart_receive_callback_t)(uint8_t data);
 
-
 void esp_usart_init(void);
+bool receive_line(char *buf, size_t maxlen);
 void esp_usart_write_data(uint8_t *data, uint16_t length);
 void esp_usart_write_string(const char *str);
 void esp_usart_receive_register(esp_usart_receive_callback_t callback);
-
 
 #endif /* __ESP_USART_H */
